956407e7f9b828de878d5e19208f05a8421a4068
arturbek92 <arturbekdjumaniyazov@gmail.com>
2024-07-18 04:01:50 +0500

    serve backend only

--- medusa-config.js
+++ medusa-config.js
@@ -46,11 +46,13 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: false,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
+      // only enable `serve` in development
+      // you may need to add the NODE_ENV variable
+      // manually
+      serve: process.env.NODE_ENV === "development",
+      // other options...
     },
+
   },
 ];
 
